072ca8f2347ef7453fa09688d55107330d388771
camilo <cvprogramacv@gmail.com>
2023-05-31 18:21:46 -0500

    Checking backend errors

--- backend/index.js
+++ backend/index.js
@@ -4,9 +4,9 @@ import config from './utils/config.js'
 import { logger } from './utils/logger.js'
 import https from 'https';
 
-app.listen(3002, () => {
-  console.log('Servidor iniciado en el puerto 3002');
-});
+// app.listen(3002, () => {
+//   console.log('Servidor iniciado en el puerto 3002');
+// });
 
 //   server.listen(config.PORT, () => {
 //   logger.info(`SERVER running on port ${config.PORT}`)
