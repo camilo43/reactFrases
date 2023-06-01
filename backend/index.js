e91b9e5e02671d92cfa27eec79ab100c4c08697d
camilo <cvprogramacv@gmail.com>
2023-06-01 05:20:19 -0500

    changing the package.Json. App is going to be the starting point instead of appx

--- backend/index.js
+++ backend/index.js
@@ -4,6 +4,7 @@ import config from './utils/config.js'
 import { logger } from './utils/logger.js'
 import https from 'https';
 
+app.use(cors())
 // app.listen(3002, () => {
 //   console.log('Servidor iniciado en el puerto 3002');
 // });
