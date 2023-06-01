b0e23ae673038c777dd0acafb959513bc34923b2
camilo <cvprogramacv@gmail.com>
2023-06-01 11:00:12 -0500

    Creating Auth page3

--- frontend/src/components/Auth.jsx
+++ frontend/src/components/Auth.jsx
@@ -1,4 +1,4 @@
-import { React, useState } from "react"
+import { React, useEffect } from "react"
 import { getUserInput } from "../axios/postAxios.js"
 import { useNavigate } from "react-router-dom"
 
