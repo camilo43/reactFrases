fb8792329c87157dc7e7755644e18136030edaeb
camilo <cvprogramacv@gmail.com>
2023-06-01 14:36:25 -0500

    Creating Auth pag7

--- backend/controllers/auth.js
+++ backend/controllers/auth.js
@@ -1,4 +1,4 @@
-import express, { response } from "express"
+import express from "express"
 
 const Auth = express.Router()
 
@@ -6,17 +6,23 @@ Auth.get("/", async(req, res) => {
     console.log("======> Signup_Router.get /AUTH");  
     const verificado = await req.cookies
     console.log("REQ TEST", req);
-    jwt.verify(verificado, process.env.KEY, (err, decodedToken) => {
-        if (err) {
-          // El token no es válido
-          console.error('Error al verificar el token:', err);
-        } else {
-          // El token es válido
-          console.log('Token verificado:', decodedToken);
-          // Puedes acceder a los datos del token decodificado en decodedToken
-          res.status(200).json({token: "accepted"})
-        }
-    })
+    try{
+        jwt.verify(verificado, process.env.KEY, (err, decodedToken) => {
+            if (err) {
+              // El token no es válido
+              console.error('Error al verificar el token:', err);
+            } else {
+              // El token es válido
+              console.log('Token verificado:', decodedToken);
+              // Puedes acceder a los datos del token decodificado en decodedToken
+              res.status(200).json({token: "accepted"})
+            }
+        })
+    }catch (error){
+        console.log("There is a problem with the Auth request")
+    }
+    
+
 })
 
 export { Auth }
