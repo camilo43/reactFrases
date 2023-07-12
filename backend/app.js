ece0c995861f30304c69ce09751f4c3f7bf7d337
camilo <cvprogramacv@gmail.com>
2023-07-12 17:29:07 +1000

    setting header

--- backend/app.js
+++ backend/app.js
@@ -15,6 +15,7 @@ const app = express()
 
 // app.use(cors())
 app.use((req, res, next) => {
+  res.setHeader('Access-Control-Allow-Origin', 'https://frontendfrases.onrender.com/');
 // res.setHeader('Access-Control-Allow-Origin', 'http://localhost:3000');
 res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE');
 res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Authorization');
