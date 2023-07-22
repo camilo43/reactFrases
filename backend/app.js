9b16a772b24876a71ed7c2bec389165f9a043e1d
camilo <cvprogramacv@gmail.com>
2023-07-22 19:03:18 +1000

    use cors origin: origin defined

--- backend/app.js
+++ backend/app.js
@@ -13,16 +13,18 @@ mongoose.set('strictQuery', false)
 
 const app = express()
 
-app.use(cors())
+app.use(cors({
+  origin:'https://backendfrases.onrender.com/'
+}))
 app.use((req, res, next) => {
   res.setHeader('Access-Control-Allow-Origin', 'https://backendfrases.onrender.com/');
-  res.setHeader('Access-Control-Allow-Origin', 'https://backendfrases.onrender.com/login');
-  res.setHeader('Access-Control-Allow-Origin', 'https://backendfrases.onrender.com/signup');
-  res.setHeader('Access-Control-Allow-Origin', 'https://backendfrases.onrender.com/auth');
-  res.setHeader('Access-Control-Allow-Origin', 'https://backendfrases.onrender.com/auth/autenticado');
-  res.setHeader('Access-Control-Allow-Origin', 'https://backendfrases.onrender.com/api/quotes');
-  res.setHeader('Access-Control-Allow-Origin', 'https://backendfrases.onrender.com/api/quotes/user');
-  res.setHeader('Access-Control-Allow-Origin', 'https://backendfrases.onrender.com/api/quotes/user/logout');
+  // res.setHeader('Access-Control-Allow-Origin', 'https://backendfrases.onrender.com/login');
+  // res.setHeader('Access-Control-Allow-Origin', 'https://backendfrases.onrender.com/signup');
+  // res.setHeader('Access-Control-Allow-Origin', 'https://backendfrases.onrender.com/auth');
+  // res.setHeader('Access-Control-Allow-Origin', 'https://backendfrases.onrender.com/auth/autenticado');
+  // res.setHeader('Access-Control-Allow-Origin', 'https://backendfrases.onrender.com/api/quotes');
+  // res.setHeader('Access-Control-Allow-Origin', 'https://backendfrases.onrender.com/api/quotes/user');
+  // res.setHeader('Access-Control-Allow-Origin', 'https://backendfrases.onrender.com/api/quotes/user/logout');
 // res.setHeader('Access-Control-Allow-Origin', 'http://localhost:3000');
 res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE');
 res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Authorization');
