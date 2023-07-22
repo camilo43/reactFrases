aca156aef6739eca58f47f015d461d7c0e612815
camilo <cvprogramacv@gmail.com>
2023-07-23 00:24:56 +1000

    Deploying again

--- backend/app.js
+++ backend/app.js
@@ -12,9 +12,11 @@ mongoose.set('strictQuery', false)
 
 const app = express()
 
-app.use(cors({ credentials: true, origin: 'http://localhost:3000' }))
+// app.use(cors({ credentials: true, origin: 'http://localhost:3000' }))
+app.use(cors({ credentials: true, origin: 'https://frontendfrases.onrender.com' }))
 app.use((req, res, next) => {
-res.setHeader('Access-Control-Allow-Origin', 'http://localhost:3000');
+// res.setHeader('Access-Control-Allow-Origin', 'http://localhost:3000');
+res.setHeader('Access-Control-Allow-Origin', 'https://frontendfrases.onrender.com');
 res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE');
 res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Authorization');
 res.setHeader('Access-Control-Allow-Credentials', 'true'); // Habilitar las credenciales
