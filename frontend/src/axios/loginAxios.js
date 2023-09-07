b656fed1de09b5d35400cbddd0834a344ae7fd4f
Juan Camilo Vega <juancvegao@iMac-de-Juan.local>
2023-09-07 21:54:14 +1000

    testing backend

--- frontend/src/axios/loginAxios.js
+++ frontend/src/axios/loginAxios.js
@@ -1,8 +1,8 @@
 import axios from "axios" 
 // axios.defaults.withCredentials = true;
 // const url = "http://localhost:3002/"
-const url = "https://backendfrases.onrender.com/"
-// const url = "https://www.camilosite/"
+// const url = "https://backendfrases.onrender.com/"
+const url = "https://backendprojects.site/"
 //PRUEBA SE SALVA LA QUE ES
 
 const postUserInput_login = async (object) => {
