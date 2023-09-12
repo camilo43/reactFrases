ea75509063b8a4353a6a4ad32629469d136b66de
Juan Camilo Vega <juancvegao@iMac-de-Juan.local>
2023-09-12 22:07:57 +1000

    x4

--- frontend/src/axios/loginAxios.js
+++ frontend/src/axios/loginAxios.js
@@ -1,13 +1,14 @@
 import axios from "axios" 
 // axios.defaults.withCredentials = true;
-// const url = "http://localhost:3002/"
+// const url = "http://localhost:3002"
 // const url = "https://backendfrases.onrender.com/"
-const url = "https://backendprojects.site"
-// const url = "https://api.camilovega.site"
+// const url = "https://www.backendprojects.site"
+const url = "https://api.camilovega.site"
 //PRUEBA SE SALVA LA QUE ES
 
 const postUserInput_login = async (object) => {
     try{ 
+        console.log("URL", url)
         const userData = await axios.post(`${url}/login`,object, {
             withCredentials: true,
             credentials: 'include'
