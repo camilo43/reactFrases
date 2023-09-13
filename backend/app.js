0c5fef3c1e85ac7f30c82e427ecfbbc11fd36123
Juan Camilo Vega <juancvegao@iMac-de-Juan.local>
2023-09-13 21:19:57 +1000

    Still working on error 307

--- backend/app.js
+++ backend/app.js
@@ -27,7 +27,7 @@ app.options('*', (req, res) => {
 app.use((req, res, next) => {
 // res.setHeader('Access-Control-Allow-Origin', 'http://localhost:3000');
 // res.setHeader('Access-Control-Allow-Origin', 'https://frontendfrases.onrender.com');
-// res.setHeader('Access-Control-Allow-Origin', 'https://camilovega.site');
+res.setHeader('Access-Control-Allow-Origin', 'https://camilovega.site');
 res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE');
 res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Authorization');
 res.setHeader('Access-Control-Allow-Credentials', 'true'); // Habilitar las credenciales
