09429dbef895f36a1717abfe12ce712f3d678d71
Juan Camilo Vega <juancvegao@iMac-de-Juan.local>
2023-09-13 22:41:37 +1000

    x5.1

--- backend/app.js
+++ backend/app.js
@@ -14,10 +14,10 @@ const app = express()
 
 // app.use(cors({ credentials: true, origin: 'http://localhost:3000' }))
 // app.use(cors({ credentials: true, origin: 'https://frontendfrases.onrender.com' }))
-app.use(cors({ credentials: true, origin: 'https://camilovega.site',methods: 'GET,HEAD,PUT,PATCH,POST,DELETE'}))
+app.use(cors({ credentials: true, origin: 'https://www.camilovega.site',methods: 'GET,HEAD,PUT,PATCH,POST,DELETE'}))
 
 app.options('*', (req, res) => {
-  res.setHeader('Access-Control-Allow-Origin', 'https://camilovega.site');
+  res.setHeader('Access-Control-Allow-Origin', 'https://www.camilovega.site');
   res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE');
   res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Authorization');
   res.setHeader('Access-Control-Allow-Credentials', 'true');
@@ -27,7 +27,7 @@ app.options('*', (req, res) => {
 app.use((req, res, next) => {
 // res.setHeader('Access-Control-Allow-Origin', 'http://localhost:3000');
 // res.setHeader('Access-Control-Allow-Origin', 'https://frontendfrases.onrender.com');
-res.setHeader('Access-Control-Allow-Origin', 'https://camilovega.site');
+res.setHeader('Access-Control-Allow-Origin', 'https://www.camilovega.site');
 res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE');
 res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Authorization');
 res.setHeader('Access-Control-Allow-Credentials', 'true'); // Habilitar las credenciales
