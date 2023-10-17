b2a578806f9293afe5365e25de029d49d8742002
Juan Camilo Vega <juancvegao@iMac-de-Juan.local>
2023-10-17 23:18:36 +1100

    v

--- frontend/src/components/Signup.jsx
+++ frontend/src/components/Signup.jsx
@@ -100,7 +100,7 @@ function Signup () {
         <div className="centerDiv">
             <div>
                 <div className="home">
-                    <button className="material-symbols-outlined" onClick={backHome}>Home</button>
+                    <button onClick={backHome}><span className="material-symbols-outlined">Home</span></button>
                 </div>
                 <h2>Signup</h2>
                 <h3 style={{color:"#b60000"}}>{displayErrorMessage}</h3>
